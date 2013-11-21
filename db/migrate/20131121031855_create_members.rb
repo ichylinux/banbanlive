ecbc5dff2d5776dff6160232372fc5cfe18ca6d7
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-21 12:22:50 +0900

    メンバーモデル修正

--- db/migrate/20131121031855_create_members.rb
+++ db/migrate/20131121031855_create_members.rb
@@ -1,7 +1,9 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-
+      t.string :last_name
+      t.string :first_name
+      t.boolean :deleted, :null => false, :default => false
       t.timestamps
     end
   end
