ec65bc3a3b0852baa02c71d26429be77fa042bbd
ichy <ichylinux@gmail.com>
2013-11-21 23:40:01 +0900

    rake db:migrate

--- db/migrate/20131121143734_create_band_logos.rb
+++ db/migrate/20131121143734_create_band_logos.rb
@@ -1,7 +1,10 @@
 class CreateBandLogos < ActiveRecord::Migration
   def change
     create_table :band_logos do |t|
-
+      t.string :file, :null => false
+      t.string :original_filename, :null => false
+      t.string :content_type, :null => false
+      t.integer :file_size, :null => false
       t.timestamps
     end
   end
