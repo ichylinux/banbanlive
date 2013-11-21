142d40aa04337c580cc56c671670b5c98c46bd34
ichy <ichylinux@gmail.com>
2013-11-21 23:34:37 +0900

    バンドのCRUD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banbanlive::Application.config.secret_token = 'dcf1f9cbb7684acb79e3b61465bee128ea04eadc24073fdd6dc4ff80a46b605e76a9b6dbcf653a88c9615e7cc1c00642f46be08f58e37e28a68089a7c0371add'
