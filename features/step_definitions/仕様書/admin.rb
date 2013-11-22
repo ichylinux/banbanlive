aef8cd73aead390807521797528cb5add13512c8
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-22 11:06:04 +0900

    メンバー管理へのリンク

--- features/step_definitions/仕様書/admin.rb
+++ features/step_definitions/仕様書/admin.rb
@@ -1,9 +1,7 @@
 # coding: UTF-8
 
-もし /^管理画面を表示する$/ do
-  assert_visit '/admin'
-end
-
 ならば /^管理メニューが表示される$/ do
-  assert page.has_selector?('ul')
+  assert page.has_selector?('ol')
+  assert page.find('ol').text.include?('メンバーの管理')
+  assert page.find('ol').text.include?('バンドの管理')
 end
