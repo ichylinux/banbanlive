a6ccd2d0d0668df5faa7b15885a1080a73f4034d
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-22 10:13:07 +0900

    merge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banbanlive::Application.config.secret_token = '66e1bb3df48b43586b2a8fdb3b7d1d28d4a3c3dad905d57c870d25ac973eb8103dcab36144bb55455ead73952df71e9be7b07f8654ed618c2c3ccbf0e46adff9'
