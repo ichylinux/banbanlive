96e7cb6591ac0f0ac3136eaef371ef5c727df73f
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-22 11:19:15 +0900

    rake dad:generate:controller lives

---
+++ app/controllers/lives_controller.rb
@@ -0,0 +1,62 @@
+# coding: UTF-8
+
+class LivesController < ApplicationController
+
+  def index
+    @lives = Live.not_deleted
+  end
+
+  def show
+    @live = Live.find(params[:id])
+  end
+
+  def new
+    @live = Live.new
+  end
+
+  def create
+    @live = Live.new(params[:live])
+    
+    begin
+      @live.transaction do
+        @live.save!
+      end
+
+      redirect_to :action => 'show', :id => @live.id
+
+    rescue ActiveRecord::RecordInvalid => e
+      render :new
+    end
+  end
+
+  def edit
+    @live = Live.find(params[:id])
+  end
+
+  def update
+    @live = Live.find(params[:id])
+    
+    begin
+      @live.transaction do
+        @live.attributes = params[:live]
+        @live.save!
+      end
+
+      redirect_to :action => 'show', :id => @live.id
+
+    rescue ActiveRecord::RecordInvalid => e
+      render :edit
+    end
+  end
+
+  def destroy
+    @live = Live.find(params[:id])
+    
+    @live.transaction do
+      @live.destroy_logically!
+    end
+    
+    redirect_to :action => 'index'
+  end
+
+end
